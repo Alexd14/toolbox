18d1c8bc676d8a3711468d0dbb21b69900c8efae
Alexd14 <32649372+Alexd14@users.noreply.github.com>
2021-10-16 15:11:48 -0400

    pypi stuff

--- setup.py
+++ setup.py
@@ -4,20 +4,21 @@ from setuptools import find_packages
 
 setup(
     name='factor-toolbox',
-    version='1.0',
+    version='1.1.2',
     packages=find_packages(),
     license='MIT',
     description='A simple toolbox to aid in the exploration of potential alpha sources.',
     author='Alex DiCarlo',
     author_email='dicarlo.a@northeastern.edu',
     url='https://github.com/Alexd14/factor-toolbox',
-    download_url='https://github.com/Alexd14/toolbox/archive/refs/tags/v1.0.tar.gz',
-    keywords=['alpha sources'],
+    download_url='https://github.com/Alexd14/toolbox/archive/refs/tags/v1.1.2.tar.gz',
+    keywords=['factor', 'equity factor'],
     install_requires=[
         'pandas',
         'pandas_market_calendars',
         'tqdm',
-        'numpy'
+        'numpy',
+        'duckdb'
     ],
     classifiers=[
         'License :: OSI Approved :: MIT License',
