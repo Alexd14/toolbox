90f6896a867129ded8c133712650e42e7c545808
Alexd14 <32649372+Alexd14@users.noreply.github.com>
2022-05-08 11:18:04 -0400

    added factor standardize code

--- toolbox/__init__.py
+++ toolbox/__init__.py
@@ -1,10 +1,16 @@
+# constitutes
 from .constitutes.constitute_adjustment import ConstituteAdjustment
+
+# utils
 from .utils.format_data_alphalens import price_format_for_alphalens, factor_format_for_alphalens
 from .utils.ml_factor_calculation import calc_ml_factor
 from .utils.ml_factor_calculation import ModelWrapper
+from .utils.utils import factorize, rank, ntile
+
+# db functions
 from .db.read.query_constructor import QueryConstructor
 from .db.api.sql_connection import SQLConnection
-from .db.read.db_functions import table_info
+from .db.read.db_functions import table_info, db_tables
 from .db.write.create_tables import IngestDataBase
 from .db.read.universe import (ETFUniverse,
                                clear_etf_universes,
@@ -18,6 +24,9 @@ __all__ = [
     'factor_format_for_alphalens',
     'calc_ml_factor',
     'ModelWrapper',
+    'factorize',
+    'rank',
+    'ntile',
     'QueryConstructor',
     'SQLConnection',
     'table_info',
@@ -26,5 +35,6 @@ __all__ = [
     'clear_etf_universes',
     'clear_built_universes',
     'BuiltUniverse',
-    'dispatch_universe_path'
+    'dispatch_universe_path',
+    'db_tables'
 ]
--- toolbox/utils/utils.py
+++ toolbox/utils/utils.py
@@ -20,7 +20,7 @@ def calculate_ic(y_true: np.array, y_pred: np.array) -> float:
     return np.corrcoef(y_true, y_pred)[0][1]
 
 
-def factorize(df: pd.DataFrame, partition_by: List[str], exclude: List[str]):
+def factorize(df: pd.DataFrame, partition_by: List[str], exclude=None):
     """
     Factorizes each column of the given dataframe except for the partition_by columns and the exclude columns
     Will preserve indexes and period data types
@@ -29,10 +29,15 @@ def factorize(df: pd.DataFrame, partition_by: List[str], exclude: List[str]):
 
     In future would like to winsorize at 2.5% and 97.5% percentiles but hard to do in sql
 
+    Won't rename the columns will overwrite them
+
     :param df: the dataframe we are factorizing
     :param partition_by: What to partition by for calculating median and std will normally be date and sector
     :param exclude: columns to exclude in the factorization process
     """
+    if exclude is None:
+        exclude = []
+
     return _duck_db_edits(df, _factorize(df, partition_by, exclude))
 
 
@@ -49,16 +54,20 @@ def _factorize(df: pd.DataFrame, partition_by: List[str], exclude: List[str]):
     return sql
 
 
-def rank(df: pd.DataFrame, partition_by: List[str], exclude: List[str], rank_type: str = 'percent_rank'):
+def rank(df: pd.DataFrame, partition_by: List[str], exclude=None, rank_type: str = 'percent_rank'):
     """
     Ranks each column of the given dataframe except for the partition_by columns and the exclude columns
     Will preserve indexes and period data types
+    Won't rename the columns will overwrite them
 
     :param df: the dataframe we are factorizing
     :param partition_by: What to partition by for calculating rank will normally be date and sector
     :param exclude: columns to exclude in the ranking process
     :param rank_type: the type of rank we are performing
     """
+    if exclude is None:
+        exclude = []
+
     return _duck_db_edits(df, _rank(df, partition_by, exclude, rank_type))
 
 
@@ -72,6 +81,35 @@ def _rank(df: pd.DataFrame, partition_by: List[str], exclude: List[str], rank_ty
                         FROM df
                         ORDER BY {', '.join(partition_by)}
                         """
+    return sql
+
+
+def ntile(df: pd.DataFrame, partition_by: List[str], exclude=None):
+    """
+    Ntiles each column of the given dataframe except for the partition_by columns and the exclude columns
+    Will preserve indexes and period data types
+    Won't rename the columns will overwrite them
+
+    :param df: the dataframe we are factorizing
+    :param partition_by: What to partition by for calculating rank will normally be date and sector
+    :param exclude: columns to exclude in the ranking process
+    :param rank_type: the type of rank we are performing
+    """
+    if exclude is None:
+        exclude = []
+
+    return _duck_db_edits(df, _ntile(df, partition_by, exclude))
+
+
+def _ntile(df, partition_by, exclude):
+    select = partition_by + exclude
+    for col in set(df.columns) - set(partition_by) - set(exclude):
+        select.append(
+            f"SELECT *, NTILE({col}) OVER(PARTITION BY {', '.join(partition_by)} ORDER BY {col} DESC) as {col} ")
+    sql = f"""SELECT {', '.join(select)}
+                            FROM df
+                            ORDER BY {', '.join(partition_by)}
+                            """
     return sql
 
 
@@ -92,3 +130,37 @@ def _duck_db_edits(df, sql):
         df[col] = df[col].dt.to_period('D')
     df = df.set_index(index_cols) if index_cols else df
     return df
+
+
+def ntile(factor: pd.Series, ntiles: int, ) -> pd.Series:
+    """
+    Universe relative Quantiles of a factor by day
+    Around 100X faster than pandas groupby qcut
+
+    pd.DataFrame of ntiled factor
+        index: (pd.Period, _asset_id)
+        Columns: (factor, ntile)
+        Values: (factor value, Ntile corresponding to factor value)
+
+    :param factor: same var as ntile_return_tearsheet
+    :param ntiles: same var as ntile_return_tearsheet
+    """
+    factor = factor.to_frame('factor').reset_index()
+    index_names = factor.columns.tolist()
+    index_names.remove('factor')
+
+    date_is_period = isinstance(factor.date.dtype, pd.core.dtypes.dtypes.PeriodDtype)
+    if date_is_period:
+        factor['date'] = factor['date'].dt.to_timestamp()
+
+    sql_quantile = f"""SELECT *, NTILE({ntiles}) OVER(PARTITION BY date ORDER BY factor.factor DESC) as ntile
+                            FROM factor
+                            WHERE factor.factor IS NOT NULL"""
+    con = duckdb.connect(':memory:')
+    factor_ntile = con.execute(sql_quantile).df()
+
+    if date_is_period:
+        factor_ntile['date'] = factor_ntile['date'].dt.to_period(freq='D')
+
+    factor_ntile = factor_ntile.set_index(index_names)
+    return factor_ntile
